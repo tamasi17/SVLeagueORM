2dee7bc4e50554345c176f009497f9a67ac2a09b
mats <mat.eidelman@gmail.com>
2026-01-16 16:57:47 +0100

    (model and jpaUtil) basic EntityManagerFactory and models for version1

---
+++ src/main/java/config/JpaUtil.java
@@ -0,0 +1,34 @@
+package config;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+/**
+ *
+ */
+public class JpaUtil {
+
+    private static final EntityManagerFactory entityManagerFactory = buildManagerFactory();
+
+    private static EntityManagerFactory buildManagerFactory() {
+
+        try {
+            return Persistence.createEntityManagerFactory("svleague");
+        } catch (Exception e) {
+            System.err.println("Error al crear EntityManagerFactory: " + e.getLocalizedMessage());
+            e.printStackTrace();
+            throw new ExceptionInInitializerError(e);
+        }
+
+    }
+
+    public static EntityManagerFactory getEntityManagerFactory(){ return entityManagerFactory;}
+
+    public static void shutdown(){
+        if (entityManagerFactory != null){
+            entityManagerFactory.close();
+        }
+    }
+
+}
