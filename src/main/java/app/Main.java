0913a3153558114fa803c65fee4c9a015d0c8ecb
mats <mat.eidelman@gmail.com>
2026-01-16 17:02:28 +0100

    (test) models added to persistence file, EntityManager created in main to test connection before starting

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,8 +1,25 @@
 package app;
 
+import config.JpaUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        // Start transaction
+
+        // Create objects
+
+        // Persist objects
+
+        // Commit
+
+        // Check to test
 
 
     }
